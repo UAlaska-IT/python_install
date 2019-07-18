85c69f8a227cddf71a8090888ebec89c0108d9d6
Caleb Severn <calsev@gmail.com>
2019-07-18 15:33:16 -0800

    Added several tests for files

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,1 +1,170 @@
 # frozen_string_literal: true
+
+node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
+
+current_ver = '3.7.4'
+prev_ver = '3.6.9'
+
+describe file('/usr/local/python-dl') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/python-bld') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/python') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe user('bud') do
+  it { should exist }
+  its('group') { should eq 'bud' }
+  its('groups') { should eq ['bud'] }
+  its('home') { should eq '/home/bud' }
+  its('shell') { should eq '/bin/bash' }
+end
+
+# Begin white-box testing of resources
+
+describe file('/var/chef') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/var/chef/cache') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}.tgz") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/usr/local/python-dl/python-#{prev_ver}.tgz") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}") do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/usr/local/python-bld/python-#{prev_ver}") do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}-dl-checksum") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{prev_ver}-dl-checksum") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}-src-checksum") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{prev_ver}-src-checksum") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}/README.md") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/usr/local/python-bld/python-#{prev_ver}/README.md") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
+
+describe file("/opt/python/#{current_ver}") do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/python') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/var/chef/cache/python-#{current_ver}/Makefile") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file("/usr/local/python-bld/python-#{prev_ver}/Makefile") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'bud' }
+  it { should be_grouped_into 'bud' }
+end
+
+# TODO: Tests for config entries
