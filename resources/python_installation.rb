e42453d682f7c2c55443b9bc78e89332acfeea60
Caleb Severn <calsev@gmail.com>
2019-07-18 15:46:57 -0800

    Added standard attributes

--- resources/python_installation.rb
+++ resources/python_installation.rb
@@ -5,6 +5,13 @@ provides :python_installation
 
 default_action :create
 
+property :version, String, default: '3280000'
+property :download_directory, [String, nil], default: nil
+property :build_directory, [String, nil], default: nil
+property :install_directory, [String, nil], default: nil
+property :owner, String, default: 'root'
+property :group, String, default: 'root'
+
 action :create do
   create_install(@new_resource)
 end
