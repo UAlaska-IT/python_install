918ad75842763c781090966e0c4763a40e15eaa2
Caleb Severn <calsev@gmail.com>
2019-07-19 18:14:22 -0800

    Enabled shared library

--- libraries/python_install_helper.rb
+++ libraries/python_install_helper.rb
@@ -91,14 +91,14 @@ module PythonInstall
     end
 
     def generate_lib_config(new_resource)
-      any_config = new_resource.openssl_directory || new_resource.sqlite_directory
       code = ''
-      code += 'export LDFLAGS="-Wl' if any_config
+      code += 'export LDFLAGS="-Wl'
       code += ",-rpath,#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += ",-rpath,#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
+      code += ",-rpath,#{File.join(install_directory, 'lib')}"
       code += " -L#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += " -L#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
-      code += "\"\n" if any_config
+      code += "\"\n"
       return code
     end
 
@@ -114,7 +114,7 @@ module PythonInstall
 
     def generate_configure_options(install_directory, new_resource)
       code = ''
-      # code += ' --enable-shared'
+      code += ' --enable-shared'
       code += " --prefix=#{install_directory}"
       code += " --exec_prefix=#{install_directory}"
       code += " --with-openssl=#{new_resource.openssl_directory}" if new_resource.openssl_directory
