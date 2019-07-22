83f3e60f9eacf8c0d19da062d8561e25a8a25ab4
Caleb Severn <calsev@gmail.com>
2019-07-22 10:29:47 -0800

    Refactored run config

--- libraries/python_install_helper.rb
+++ libraries/python_install_helper.rb
@@ -90,12 +90,18 @@ module PythonInstall
       return ''
     end
 
-    def generate_lib_config(install_directory, new_resource)
+    def generate_run_config(install_directory, new_resource)
       code = ''
-      code += 'export LDFLAGS="-Wl'
       code += ",-rpath,#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += ",-rpath,#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
       code += ",-rpath,#{File.join(install_directory, 'lib')}"
+      return code
+    end
+
+    def generate_lib_config(install_directory, new_resource)
+      code = ''
+      code += 'export LDFLAGS="-Wl'
+      code += generate_run_config(install_directory, new_resource)
       code += " -L#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += " -L#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
       code += "\"\n"
