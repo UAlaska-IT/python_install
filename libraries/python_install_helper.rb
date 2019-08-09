c16eade790cd1a34d56a441049e39d0654e673b2
Caleb Severn <calsev@gmail.com>
2019-08-09 15:45:05 -0800

    Adjusted to reliable install directory

--- libraries/python_install_helper.rb
+++ libraries/python_install_helper.rb
@@ -34,77 +34,77 @@ module PythonInstall
       return "bin/python#{revision}"
     end
 
-    def path_to_python_binary(install_directory, new_resource)
-      return File.join(install_directory, rel_path_to_python_binary(new_resource))
+    def path_to_python_binary(new_resource)
+      return File.join(new_resource.install_directory, rel_path_to_python_binary(new_resource))
     end
 
-    def path_to_pip_binary(install_directory, new_resource)
+    def path_to_pip_binary(new_resource)
       revision = python_revision(new_resource)
-      return File.join(install_directory, "bin/pip#{revision}")
+      return File.join(new_resource.install_directory, "bin/pip#{revision}")
     end
 
-    def make_python_link(install_directory, new_resource)
+    def make_python_link(new_resource)
       link 'Python Link' do
-        target_file File.join(install_directory, 'bin/python')
-        to path_to_python_binary(install_directory, new_resource)
+        target_file File.join(new_resource.install_directory, 'bin/python')
+        to path_to_python_binary(new_resource)
         owner new_resource.owner
         group new_resource.group
       end
     end
 
-    def make_python3_link(install_directory, new_resource)
+    def make_python3_link(new_resource)
       link 'Python3 Link' do
-        target_file File.join(install_directory, 'bin/python3')
-        to path_to_python_binary(install_directory, new_resource)
+        target_file File.join(new_resource.install_directory, 'bin/python3')
+        to path_to_python_binary(new_resource)
         owner new_resource.owner
         group new_resource.group
       end
     end
 
-    def make_python_links(install_directory, new_resource)
-      make_python_link(install_directory, new_resource)
-      make_python3_link(install_directory, new_resource)
+    def make_python_links(new_resource)
+      make_python_link(new_resource)
+      make_python3_link(new_resource)
     end
 
-    def make_pip_link(install_directory, new_resource)
+    def make_pip_link(new_resource)
       link 'Pip Link' do
-        target_file File.join(install_directory, 'bin/pip')
-        to path_to_pip_binary(install_directory, new_resource)
+        target_file File.join(new_resource.install_directory, 'bin/pip')
+        to path_to_pip_binary(new_resource)
         owner new_resource.owner
         group new_resource.group
       end
     end
 
-    def make_pip3_link(install_directory, new_resource)
+    def make_pip3_link(new_resource)
       link 'Pip3 Link' do
-        target_file File.join(install_directory, 'bin/pip3')
-        to path_to_pip_binary(install_directory, new_resource)
+        target_file File.join(new_resource.install_directory, 'bin/pip3')
+        to path_to_pip_binary(new_resource)
         owner new_resource.owner
         group new_resource.group
       end
     end
 
-    def make_pip_links(install_directory, new_resource)
-      make_pip_link(install_directory, new_resource)
-      make_pip3_link(install_directory, new_resource)
+    def make_pip_links(new_resource)
+      make_pip_link(new_resource)
+      make_pip3_link(new_resource)
     end
 
     def generate_bin_config(_install_directory, _new_resource)
       return ''
     end
 
-    def generate_run_config(install_directory, new_resource)
+    def generate_run_config(new_resource)
       code = ''
       code += ",-rpath,#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += ",-rpath,#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
-      code += ",-rpath,#{File.join(install_directory, 'lib')}"
+      code += ",-rpath,#{File.join(new_resource.install_directory, 'lib')}"
       return code
     end
 
-    def generate_lib_config(install_directory, new_resource)
+    def generate_lib_config(new_resource)
       code = ''
       code += 'export LDFLAGS="-Wl'
-      code += Custom.generate_run_config(install_directory, new_resource)
+      code += Custom.generate_run_config(new_resource)
       code += " -L#{openssl_lib_directory(new_resource)}" if new_resource.openssl_directory
       code += " -L#{sqlite_lib_directory(new_resource)}" if new_resource.sqlite_directory
       code += "\"\n"
@@ -121,11 +121,11 @@ module PythonInstall
       return code
     end
 
-    def generate_configure_options(install_directory)
+    def generate_configure_options(new_resource)
       code = ''
       code += ' --enable-shared'
-      code += " --prefix=#{install_directory}"
-      code += " --exec_prefix=#{install_directory}"
+      code += " --prefix=#{new_resource.install_directory}"
+      code += " --exec_prefix=#{new_resource.install_directory}"
       code += ' --with-system-ffi'
       code += ' --enable-optimizations'
       return code
@@ -158,12 +158,12 @@ module PythonInstall
       return 'README.rst'
     end
 
-    def configuration_command(install_directory, new_resource)
-      code = Custom.generate_bin_config(install_directory, new_resource)
-      code += Custom.generate_lib_config(install_directory, new_resource)
+    def configuration_command(new_resource)
+      code = Custom.generate_bin_config(new_resource)
+      code += Custom.generate_lib_config(new_resource)
       code += Custom.generate_inc_config(new_resource)
       code += ' ./configure'
-      code += Custom.generate_configure_options(install_directory)
+      code += Custom.generate_configure_options(new_resource)
       return code
     end
 
@@ -175,9 +175,9 @@ module PythonInstall
       return 'make altinstall'
     end
 
-    def post_install_logic(install_directory, new_resource)
-      Custom.make_python_links(install_directory, new_resource)
-      Custom.make_pip_links(install_directory, new_resource)
+    def post_install_logic(new_resource)
+      Custom.make_python_links(new_resource)
+      Custom.make_pip_links(new_resource)
     end
 
     # For common install code see base_install cookbook
